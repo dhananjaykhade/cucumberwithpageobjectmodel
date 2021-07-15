c8f96121e70b75fcf52f73409edced3c15876ac8
Dhananjay <Dhananjay K@Dhananjay>
2021-07-15 20:50:49 +0530

    Added another steps

--- facebookcucumber/Features/Login.feature
+++ facebookcucumber/Features/Login.feature
@@ -4,3 +4,4 @@ Feature: User wants to Login Functionality
   @SanityTest
   Scenario: Check Successfull login
     Given user is on login page
+    When user enters username and password
--- facebookcucumber/src/test/java/com/cas/glue/stepdefinition/steps.java
+++ facebookcucumber/src/test/java/com/cas/glue/stepdefinition/steps.java
@@ -6,13 +6,19 @@ import com.cas.framework.Keywords;
 import com.cas.tests.pageobject.LoginPage;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class steps {
 	@Given("user is on login page")
 	public void user_is_on_login_page() {
 		Keywords.openBrowser();
 		Keywords.launchUrl();
+		Keywords.maximizeWindow();
 
+	}
+
+	@When("user enters username and password")
+	public void user_enter_uname_pass() {
 		LoginPage lp = PageFactory.initElements(Keywords.driver, LoginPage.class);
 		lp.enterUname("Test@ymail.com");
 		lp.enterPass("Admin321@");
